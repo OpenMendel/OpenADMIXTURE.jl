86f2eeb1db6677643bd4f932cfae020e19392b49
Seyoon Ko <kos@ucla.edu>
2023-09-07 15:16:37 -0700

    fix doc

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ makedocs(;
     modules=[OpenADMIXTURE],
     authors="Seyoon Ko <kos@ucla.edu> and contributors",
     repo="https://github.com/OpenMendel/OpenADMIXTURE.jl/blob/{commit}{path}#L{line}",
-    sitename="SKFR.jl",
+    sitename="OpenADMIXTURE.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://OpenMendel.github.io/OpenADMIXTURE.jl",
