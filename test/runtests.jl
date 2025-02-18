79aeba0b81d22f263e936bf9ae55bed4dc54b86e
Seyoon Ko <y3kkoseyoon@gmail.com>
2025-02-17 18:01:15 -0800

    change rng for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,14 @@
-using OpenADMIXTURE, SnpArrays, Random
+using OpenADMIXTURE, SnpArrays, Random, StableRNGs
 using Test
 
 @testset "OpenADMIXTURE.jl" begin
     EUR = SnpArrays.datadir("EUR_subset.bed")
-    rng = MersenneTwister(7856)
+    rng = StableRNG(7856)
     d, _, _ = OpenADMIXTURE.run_admixture(EUR, 4; rng=rng)
-    @test d.ll_new ≈ -1.555219392972111e7
-    rng = MersenneTwister(7856)
+    @test d.ll_new ≈ -1.555219448773069e7
+    rng = StableRNG(7856)
     d, _, _ = OpenADMIXTURE.run_admixture(EUR, 4; sparsity=1000, rng=rng, prefix="test")
-    @test d.ll_new ≈ -216519.78193892565
+    @test d.ll_new ≈ -235919.4556341082
     rm("test_4_1000aims.bed", force=true)
     rm("test_4_1000aims.bim", force=true)
     rm("test_4_1000aims.fam", force=true)
